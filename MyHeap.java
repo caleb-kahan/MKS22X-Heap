c1bb3dd7c9cc8f5476bdc82065d503c00829746b
Caleb Kahan <ckahan00@stuy.edu>
2019-04-21 22:55:11 -0400

    copied and did swap

---
+++ MyHeap.java
@@ -0,0 +1,19 @@
+public class MyHeap{
+  private static void pushDown(int[]data,int size,int index){
+
+  }
+  private static void pushUp(int[]data,int index){
+
+  }
+  public static void heapify(int[] data){
+
+  }
+  public static void heapsort(int[] data){
+
+  }
+  public static void swap(int[] data, int index1, int index2){
+    int temp = data[index1];
+    data[index1] = data[index2];
+    data[index2]= temp;
+  }
+}
